210fc858aba1384302accd15ee0c5dbc5ecb29d4
1KimJiHyeon <mycasio1@mju.ac.kr>
2020-12-31 04:10:30 +0900

    Planning the Movie Component

--- ReactJS/movie_app/src/App.js
+++ ReactJS/movie_app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isloading : true,
+    movies : []
+  };
+  
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isloading : false});
+    },6000);
+  }
+
+  render(){
+    const {isloading} = this.state
+    return (<div>
+      {isloading ? "Loading" : "We are ready"}
+    </div>);
+  }
 }
-
 export default App;
