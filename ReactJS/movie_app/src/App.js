e096130be6c81ebffc0f0d54bc5bd22335dd76ff
1KimJiHyeon <mycasio1@mju.ac.kr>
2021-01-01 23:17:57 +0900

    Styling the Movies

--- ReactJS/movie_app/src/App.js
+++ ReactJS/movie_app/src/App.js
@@ -1,46 +1,51 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
-    isloading: true,
+    isLoading: true,
     movies: []
   };
-
   getMovies = async () => {
     const {
       data: {
-        data: {
-          movies
-        }
+        data: { movies }
       }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    this.setState({ isloading: false, movies: movies });
-  }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
   componentDidMount() {
     this.getMovies();
   }
-
   render() {
-    const { isloading, movies } = this.state
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        {
-          isloading
-            ? "Loading"
-            : movies.map(movie => (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
-                poster={movie.medium_cover_image} />
-            ))
-        }
-      </div>
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
+
 export default App;
