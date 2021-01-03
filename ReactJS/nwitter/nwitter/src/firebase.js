ac6eb1a90a108f18ab2d2c69820470a4cb30acfe
1KimJiHyeon <mycasio1@mju.ac.kr>
2021-01-03 22:33:22 +0900

    React + Firebase Setup

---
+++ ReactJS/nwitter/nwitter/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwbKpL2OBzSVx_BObzQ1SauC6wRFyQOCA",
+    authDomain: "nwitter-3d8d1.firebaseapp.com",
+    projectId: "nwitter-3d8d1",
+    storageBucket: "nwitter-3d8d1.appspot.com",
+    messagingSenderId: "566418622991",
+    appId: "1:566418622991:web:c581ec98ab28dfb238f17a",
+    measurementId: "G-MB9V1E3NQ1"
+  };
+
+
+export default firebase.initializeApp(firebaseConfig);
